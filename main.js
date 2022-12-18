bc6a26fa7ebe0a6ede38e34cd7704d279872c7cc
klu0926 <106499794+klu0926@users.noreply.github.com>
2022-12-18 14:36:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -23,22 +23,66 @@ const commentsAPI = "https://www.googleapis.com/youtube/v3/commentThreads?"
 const API_KEY = 'AIzaSyC2UIxuyuVcI4-tShB6QY6gjN151eM9zPI';
 let videoId = '';
 
+// Model
 // video name list
+let commentsArray = []
 let nameList = [] 
 
 
 
 // 選出贏家！
 document.querySelector("#winnerButton").addEventListener("click", event => {
+
+    hideComment()
+
     if (nameList.length === 0){
         alert("你還沒有輸入影片網址唷！")
     } else {
-        const number = Math.floor( Math.random() * nameList.length)
-        const winner = nameList[number]
-        document.querySelector("#winner").innerText = winner
-    }
-})
+        const myInterval = setInterval(myWinnerDisplay, 70);
+        // set up 
+        const spinTimes = 20
+        let  i = 0
+        let number = 0
+        let winner = ""
 
+        // pick winner
+        function myWinnerDisplay() {
+                        i ++
+            if (i > spinTimes){
+                clearInterval(myInterval)
+                displayComment()
+            } else {
+                number = Math.floor( Math.random() * nameList.length)
+                winner = nameList[number]
+                document.querySelector("#winner").innerText = winner
+            }
+        }
+        // display winner's commments
+        function displayComment(){
+            const message = commentsArray[number].snippet.topLevelComment.snippet.textDisplay
+
+            // show comment
+            document.querySelector("#winnerCommentLabel").style.display = "block"
+            document.querySelector("#winnerComment").textContent = message
+        }
+    }
+}) 
+
+// hide comment
+function hideComment(){
+    document.querySelector("#winnerCommentLabel").style.display = "none"
+    document.querySelector("#winnerComment").textContent = ""
+}
+
+//hide winner 
+function hideWinner(){
+    document.querySelector("#winner").textContent = ""
+}
+
+// reload button
+document.querySelector("#reloadButton").addEventListener("click", (event) => {
+    document.location.reload();
+})
 
 // 尋找留言小夥伴！
 document.querySelector("#sendButton").addEventListener("click", (event) => {
@@ -47,6 +91,10 @@ document.querySelector("#sendButton").addEventListener("click", (event) => {
     // event.preventDefault()
     // get the input
     const inputValue = document.querySelector("#urlInput").value
+
+    // hide comment & winner
+    hideWinner()
+    hideComment()
 
     // get the videoID from the video url
     const regex = /v=([^&]+)/
@@ -80,7 +128,7 @@ document.querySelector("#sendButton").addEventListener("click", (event) => {
         document.querySelector("#title").innerHTML = videoTitle
 
         // get commentor object
-        let commentsArray = await commentsData.items
+        commentsArray = await commentsData.items
         
         // reset name list
         nameList = []
@@ -95,6 +143,7 @@ document.querySelector("#sendButton").addEventListener("click", (event) => {
 
         // display commentor's names
         nameList.map(name => {
+            // create a new <span> element
             const sp = document.createElement("span")
             sp.innerText = name
             sp.classList.add("name-tag","bg-primary","text-white")
