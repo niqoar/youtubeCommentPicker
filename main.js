a3c3419c9d4e8da46d087ffe4aa3ffb5c9a0b6ff
niqoar <niqo-cs@hotmail.com>
2024-07-19 19:05:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,157 +16,119 @@ A screen will appear with the API key.
 //Youtube API HOW TO: //
 //https://developers.google.com/youtube/v3/code_samples/code_snippets//
 
-
 // Youtube API
-const snipitsAPI= "https://www.googleapis.com/youtube/v3/videos?part=snippet&id="
-const commentsAPI = "https://www.googleapis.com/youtube/v3/commentThreads?"
+const snipitsAPI = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=";
+const commentsAPI = "https://www.googleapis.com/youtube/v3/commentThreads?";
 const API_KEY = 'AIzaSyC2UIxuyuVcI4-tShB6QY6gjN151eM9zPI';
 let videoId = '';
 
 // Model
-// video name list
-let commentsArray = []
-let nameList = [] 
+let commentsArray = [];
+let nameList = [];
 
-
-
-// 選出贏家！
+// Choose a winner
 document.querySelector("#winnerButton").addEventListener("click", event => {
+    hideComment();
 
-    hideComment()
-
-    if (nameList.length === 0){
-        alert("¡Ingresa el link del video!")
+    if (nameList.length === 0) {
+        alert("¡Ingresa el link del video!");
     } else {
-        // set up 
-        const spinTimes = 20
-        let  i = 0
-        let number = 0
-        let winner = ""
+        const spinTimes = 20;
+        let i = 0;
+        let number = 0;
+        let winner = "";
 
-        // pick winner
         const myInterval = setInterval(myWinnerDisplay, 70);
 
-        //function pick winner
         function myWinnerDisplay() {
-                        i ++
-            if (i > spinTimes){
-                clearInterval(myInterval)
-                displayComment()
+            i++;
+            if (i > spinTimes) {
+                clearInterval(myInterval);
+                displayComment();
             } else {
-                number = Math.floor( Math.random() * nameList.length)
-                winner = nameList[number]
-                document.querySelector("#winner").innerText = winner
+                number = Math.floor(Math.random() * nameList.length);
+                winner = nameList[number];
+                document.querySelector("#winner").innerText = winner;
             }
         }
-        // display winner's commments
-        function displayComment(){
-            const message = commentsArray[number].snippet.topLevelComment.snippet.textDisplay
 
-            // show comment
-            document.querySelector("#winnerCommentLabel").style.display = "block"
-            document.querySelector("#winnerComment").innerHTML = message
+        function displayComment() {
+            const message = commentsArray[number].snippet.topLevelComment.snippet.textDisplay;
+            document.querySelector("#winnerCommentLabel").style.display = "block";
+            document.querySelector("#winnerComment").innerHTML = message;
         }
     }
-}) 
+});
 
-// hide comment
-function hideComment(){
-    document.querySelector("#winnerCommentLabel").style.display = "none"
-    document.querySelector("#winnerComment").textContent = ""
+function hideComment() {
+    document.querySelector("#winnerCommentLabel").style.display = "none";
+    document.querySelector("#winnerComment").textContent = "";
 }
 
-//hide winner 
-function hideWinner(){
-    document.querySelector("#winner").textContent = ""
+function hideWinner() {
+    document.querySelector("#winner").textContent = "";
 }
 
-// reload button
 document.querySelector("#reloadButton").addEventListener("click", (event) => {
     document.location.reload();
-})
+});
 
-
-
-// 尋找留言小夥伴！
-//
 document.querySelector("#sendButton").addEventListener("click", (event) => {
-    // prevent the default form submission behavior and retrieve the value of the URL input field.
-    // without preventDefault() you can't get the value in input
-    // event.preventDefault()
-    // get the input
-    const inputValue = document.querySelector("#urlInput").value
+    const inputValue = document.querySelector("#urlInput").value;
+    hideWinner();
+    hideComment();
 
-    // hide comment & winner
-    hideWinner()
-    hideComment()
-
-    // get the videoID from the video url
-    // 兩種，一個是youtbue網頁，一個是youtube APP
     let regex = /v=([^&]+)|be\/([^&]+)/;
     const match = inputValue.match(regex);
     videoId = match[1] || match[2];
 
-    // match=( whole match,(group1),(group2) )
-    // 參考文件：https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/String/match
-    console.log(`match =${match}`)
-    console.log(`match lenght=${match.length}`)
-    console.log(`videoID =${videoId}`)
+    console.log(`match =${match}`);
+    console.log(`match length=${match.length}`);
+    console.log(`videoID =${videoId}`);
 
-    // API for video info
     const URL = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
-    console.log(URL)
 
-    // API for comment
-    const maxResult = 300
-    const URL2 = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=${maxResult}&order=time&videoId=${videoId}&key=${API_KEY}`
-    console.log(URL2)
+    const maxResult = 300;
+    const URL2 = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=${maxResult}&order=time&videoId=${videoId}&key=${API_KEY}`;
 
-    getData()
+    getData(URL, URL2);
 
-        // in a asyn function, we can use the await method
-        async function getData() {
-        //await: fetch API response 
-        const responseInfo = await fetch(URL);
-        const responseComments = await fetch(URL2)
-        //await: conver response's promist to a json object call data
+    async function getData(videoURL, commentsURL, nextPageToken = '') {
+        const responseInfo = await fetch(videoURL);
+        let finalCommentsURL = commentsURL;
+        if (nextPageToken) {
+            finalCommentsURL += `&pageToken=${nextPageToken}`;
+        }
+        const responseComments = await fetch(finalCommentsURL);
         const infoData = await responseInfo.json();
         const commentsData = await responseComments.json();
 
-        // get video's channel title
         const videoTitle = infoData.items[0].snippet.title;
-        
-        // display channel title
-        document.querySelector("#title").innerHTML = videoTitle
+        document.querySelector("#title").innerHTML = videoTitle;
 
-        // get commentor object
-        commentsArray = await commentsData.items
-        
-        // reset name list
-        nameList = []
-        // reset name list disply
-        document.querySelector("#nameList").innerHTML = ""
+        commentsArray = [...commentsArray, ...commentsData.items];
 
-        // reset name tot
-        document.querySelector("#nameTot").innerText = `Usuarios:`
-        
-        // get comentor's names
-        commentsArray.map(item => { 
-            nameList.push(item.snippet.topLevelComment.snippet.authorDisplayName)
-        })
-        console.log(`nameList = ${nameList}`)
+        if (commentsData.nextPageToken) {
+            await getData(videoURL, commentsURL, commentsData.nextPageToken);
+        } else {
+            nameList = [];
+            document.querySelector("#nameList").innerHTML = "";
+            document.querySelector("#nameTot").innerText = `Usuarios:`;
 
-        // display commentor's names
-        nameList.map(name => {
-            // create a new <span> element
-            const sp = document.createElement("span")
-            sp.innerText = name
-            sp.classList.add("name-tag","bg-primary","text-white")
-            document.querySelector("#nameList").appendChild(sp)
-        })
+            commentsArray.map(item => {
+                nameList.push(item.snippet.topLevelComment.snippet.authorDisplayName);
+            });
+            console.log(`nameList = ${nameList}`);
 
-        // display total number of users
-        console.log(`nameTot = ${nameList.length}`)
-        document.querySelector("#nameTot").innerText = `Usuarios: ${nameList.length} en total`
+            nameList.map(name => {
+                const sp = document.createElement("span");
+                sp.innerText = name;
+                sp.classList.add("name-tag", "bg-primary", "text-white");
+                document.querySelector("#nameList").appendChild(sp);
+            });
+
+            console.log(`nameTot = ${nameList.length}`);
+            document.querySelector("#nameTot").innerText = `Usuarios: ${nameList.length} en total`;
+        }
     }
-})
+});
